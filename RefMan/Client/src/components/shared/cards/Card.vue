2cd4cd703b6891be0f0d596c0272d7846988282d
Aleksbgbg <kozichka01@abv.bg>
2019-11-24 17:49:02 +0000

    [Card.vue] Add card component with grey outline.

---
+++ RefMan/Client/src/components/shared/cards/Card.vue
@@ -0,0 +1,4 @@
+<template lang="pug">
+.rounded.border.border-solid.border-gray-300.p-4
+  slot
+</template>
--- RefMan/Client/src/main.ts
+++ RefMan/Client/src/main.ts
@@ -5,10 +5,12 @@ import router from "./router";
 import "@/assets/tailwind.css";
 
 import ButtonComponent from "@/components/shared/buttons/Button.vue";
+import CardComponent from "@/components/shared/cards/Card.vue";
 import DropdownButtonComponent from "@/components/shared/dropdowns/DropdownButton.vue";
 import DropdownItemComponent from "@/components/shared/dropdowns/DropdownItem.vue";
 
 Vue.component("c-button", ButtonComponent);
+Vue.component("c-card", CardComponent);
 Vue.component("c-dropdown-button", DropdownButtonComponent);
 Vue.component("c-dropdown-item", DropdownItemComponent);
 
