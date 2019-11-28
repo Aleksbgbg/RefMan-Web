f042ad6223d4427f317a194ad3c8b606760f5755
Aleksbgbg <kozichka01@abv.bg>
2019-11-28 08:20:56 +0000

    [GlobalComponents] Move global component registration to separate file.

--- RefMan/Client/src/main.ts
+++ RefMan/Client/src/main.ts
@@ -4,15 +4,7 @@ import router from "./router";
 
 import "@/assets/tailwind.css";
 
-import ButtonComponent from "@/components/shared/buttons/Button.vue";
-import CardComponent from "@/components/shared/cards/Card.vue";
-import DropdownButtonComponent from "@/components/shared/dropdowns/DropdownButton.vue";
-import DropdownItemComponent from "@/components/shared/dropdowns/DropdownItem.vue";
-
-Vue.component("c-button", ButtonComponent);
-Vue.component("c-card", CardComponent);
-Vue.component("c-dropdown-button", DropdownButtonComponent);
-Vue.component("c-dropdown-item", DropdownItemComponent);
+import "./GlobalComponents";
 
 Vue.config.productionTip = false;
 
