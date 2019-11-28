19c0f687a567b2f2645fb5cc39ceb0195d0b03af
Aleksbgbg <kozichka01@abv.bg>
2019-11-28 08:08:04 +0000

    [UsernameInput.vue] Add form-input component that has base username props set.

---
+++ RefMan/Client/src/components/auth/inputs/UsernameInput.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+c-form-input(
+  name="username"
+  title="Username"
+  type="text"
+  valid-state-message="Your username is valid."
+  v-bind="$attrs"
+  v-on="forwardInputListeners"
+)
+</template>
+
+<script>
+import NestedInputForwarderMixin from "@/mixins/input-forwarder/NestedInputForwarder";
+import FormInputComponent from "@/components/shared/forms/FormInput";
+
+export default {
+  inheritAttrs: false,
+  mixins: [NestedInputForwarderMixin],
+  components: {
+    "c-form-input": FormInputComponent
+  }
+};
+</script>
