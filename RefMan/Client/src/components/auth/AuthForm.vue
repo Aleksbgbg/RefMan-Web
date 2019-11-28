68ba2c8839096eddf614a0594787213aca53b3f4
Aleksbgbg <kozichka01@abv.bg>
2019-11-28 08:05:53 +0000

    [AuthForm.vue] Add component for login/register form that encapsulates basic layout.

---
+++ RefMan/Client/src/components/auth/AuthForm.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+c-card.max-w-xs.mx-auto.mt-4
+  h4.text-2xl {{ title }}
+  c-form(@submit="$emit('submit')")
+    slot
+    c-form-submit-button
+</template>
+
+<script>
+import FormComponent from "@/components/shared/forms/Form";
+import FormSubmitButtonComponent from "@/components/shared/forms/FormSubmitButton";
+
+export default {
+  components: {
+    "c-form": FormComponent,
+    "c-form-submit-button": FormSubmitButtonComponent
+  },
+  props: {
+    title: String
+  }
+};
+</script>
