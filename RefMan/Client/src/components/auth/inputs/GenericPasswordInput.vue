9dddaa4d18cd3f8d2a80838857d8edcdb5318df0
Aleksbgbg <kozichka01@abv.bg>
2019-11-28 08:08:24 +0000

    [GenericPasswordInput.vue] Add form-input component that has base password props set.

---
+++ RefMan/Client/src/components/auth/inputs/GenericPasswordInput.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+c-form-input(
+  type="password"
+  v-bind="$attrs"
+  v-on="forwardInputListeners"
+)
+</template>
+
+<script>
+import NestedInputForwarderMixin from "@/mixins/input-forwarder/NestedInputForwarder";
+import FormInputComponent from "@/components/shared/forms/FormInput";
+
+export default {
+  inheritAttrs: false,
+  mixins: [NestedInputForwarderMixin],
+  components: {
+    "c-form-input": FormInputComponent
+  }
+};
+</script>
