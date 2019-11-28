3acfa9ed916eafb362c1dc5898919873254d6a7f
Aleksbgbg <kozichka01@abv.bg>
2019-11-28 08:11:47 +0000

    [Login.vue] Create login component using form building blocks.

---
+++ RefMan/Client/src/components/auth/Login.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+auth-form(title="Login" @submit="submit")
+  username-input(
+    placeholder="Enter your username"
+    v-model="username.value"
+    :validation-state="username.isValid"
+    :invalid-state-message="username.invalidMessage"
+  )
+  password-input(
+    placeholder="Enter your password"
+    v-model="password.value"
+    :validation-state="password.isValid"
+    :invalid-state-message="password.invalidMessage"
+  )
+</template>
+
+<script>
+import AuthFormComponent from "./AuthForm";
+import UsernameInputComponent from "./inputs/UsernameInput";
+import PasswordInputComponent from "./inputs/PasswordInput";
+import { generateStub } from "@/utilities/FormDataStubGenerator";
+
+export default {
+  components: {
+    "auth-form": AuthFormComponent,
+    "username-input": UsernameInputComponent,
+    "password-input": PasswordInputComponent
+  },
+  data() {
+    return {
+      username: generateStub(),
+      password: generateStub()
+    };
+  },
+  methods: {
+    submit() {
+    }
+  }
+};
+</script>
