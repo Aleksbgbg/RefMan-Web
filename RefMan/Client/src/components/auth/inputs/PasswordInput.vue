32f23f0db77c62ffc0424d95ea9f80168ee07d3f
Aleksbgbg <kozichka01@abv.bg>
2019-11-28 08:09:08 +0000

    [PasswordInput.vue] Create regular password component, building on generic-password-input.

---
+++ RefMan/Client/src/components/auth/inputs/PasswordInput.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+generic-password-input(
+  name="password"
+  title="Password"
+  valid-state-message="Your password is valid."
+  v-bind="$attrs"
+  v-on="forwardInputListeners"
+)
+</template>
+
+<script>
+import NestedInputForwarderMixin from "@/mixins/input-forwarder/NestedInputForwarder";
+import GenericPasswordInputComponent from "./GenericPasswordInput";
+
+export default {
+  inheritAttrs: false,
+  mixins: [NestedInputForwarderMixin],
+  components: {
+    "generic-password-input": GenericPasswordInputComponent
+  }
+};
+</script>
