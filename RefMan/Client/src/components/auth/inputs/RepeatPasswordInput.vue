a1421619d4872e2581824c19c0a76269d94f0108
Aleksbgbg <kozichka01@abv.bg>
2019-11-28 08:09:54 +0000

    [RepeatPasswordInput.vue] Create repeat password component, buildin on generic-password-input.

---
+++ RefMan/Client/src/components/auth/inputs/RepeatPasswordInput.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+generic-password-input(
+  name="repeat-password"
+  title="Repeat Password"
+  placeholder="Repeat the above password"
+  valid-state-message="Your passwords match!"
+  v-bind="$attrs"
+  v-on="forwardInputListeners"
+)
+</template>
+
+<script>
+import NestedInputForwarderMixin from "@/mixins/input-forwarder/NestedInputForwarder";
+import GenericPasswordInputComponent from "./GenericPasswordInput";
+
+export default {
+  inheritAttrs: false,
+  mixins: [NestedInputForwarderMixin],
+  components: {
+    "generic-password-input": GenericPasswordInputComponent
+  }
+};
+</script>
