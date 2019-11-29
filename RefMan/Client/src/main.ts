ca6f10adc7324e667c19b630364ab05b2761b5f2
Aleksbgbg <kozichka01@abv.bg>
2019-11-29 21:03:42 +0000

    [main] Check user login on startup via vuex store.

--- RefMan/Client/src/main.ts
+++ RefMan/Client/src/main.ts
@@ -2,12 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import { actionTypes } from "@/store/account/Types";
 import "@/assets/tailwind.css";
-
 import "./GlobalComponents";
 
 Vue.config.productionTip = false;
+
+(async function() {
+  await store.dispatch(actionTypes.CHECK_LOGIN);
+})();
 
 new Vue({
   router,
