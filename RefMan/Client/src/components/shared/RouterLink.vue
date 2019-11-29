5b6276f62d9ebf69b8f106998e4250ef1205dee7
Aleksbgbg <kozichka01@abv.bg>
2019-11-29 19:52:48 +0000

    [RouterLink.vue] Add blue-coloured router-link component.

---
+++ RefMan/Client/src/components/shared/RouterLink.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+router-link.text-blue-500.hover-text-blue-700.hover-underline(v-bind="$attrs")
+  slot
+</template>
+
+<script>
+export default {
+  inheritAttrs: false
+};
+</script>
