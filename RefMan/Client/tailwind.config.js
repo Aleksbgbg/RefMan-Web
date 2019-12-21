453bb387fc5ad0193fe4f2a0524f088d009cb8b2
Aleksbgbg <kozichka01@abv.bg>
2019-12-21 12:51:07 +0000

    [tailwind.config.js] Add w-px styles in increments of 100px.

--- RefMan/Client/tailwind.config.js
+++ RefMan/Client/tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
       colors: {
         "dark-light": "#343a40",
         "dark-dark": "#2a2c2e"
+      },
+      width: {
+        "px-100": "100px",
+        "px-200": "200px",
+        "px-300": "300px",
+        "px-400": "400px",
+        "px-500": "500px",
+        "px-600": "600px",
+        "px-700": "700px",
+        "px-800": "800px",
+        "px-900": "900px"
       }
     }
   },
