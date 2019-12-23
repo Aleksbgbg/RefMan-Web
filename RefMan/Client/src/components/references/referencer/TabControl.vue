0a7e9affd6f2f4c8696d12daf9d9ec485bc736b9
Aleksbgbg <kozichka01@abv.bg>
2019-12-23 22:15:12 +0000

    [TabControl.vue] Add generic tab control component for file editor.

---
+++ RefMan/Client/src/components/references/referencer/TabControl.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+.flex.border-b-2
+  template(v-for="item of items")
+    c-tab-item(
+      :key="item.id"
+      :item="item"
+      :isSelected="item.id === selectedItem.id"
+      v-slot="{ isSelected }"
+      @selected="selectedItem = item"
+      @close="onClose"
+    )
+      slot(
+        name="header"
+        :item="item"
+        :isSelected="isSelected"
+      )
+</template>
+
+<script>
+import TabItemComponent from "@/components/references/referencer/TabItem";
+
+export default {
+  components: {
+    "c-tab-item": TabItemComponent
+  },
+  props: {
+    items: Array
+  },
+  data() {
+    return {
+      selectedItem: this.items[0]
+    };
+  },
+  watch: {
+    items() {
+      if (this.items.length > 0 && this.selectedItem === null) {
+        this.selectedItem = this.items[0];
+      }
+    }
+  },
+  methods: {
+    onClose(item) {
+      this.$emit("close", item);
+
+      if (this.items.length === 0) {
+        this.selectedItem = null;
+      } else if (item === this.selectedItem) {
+        this.selectedItem = this.items[0];
+      }
+    }
+  }
+};
+</script>
---
+++ RefMan/Client/src/components/references/referencer/TabItem.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+.flex.items-center.border-b-4.hover-bg-gray-200.px-2(
+  :class="isSelected ? 'border-blue-400' : 'border-transparent'"
+  @click="onSelect"
+)
+  slot(:isSelected="isSelected")
+  c-font-awesome-icon.hover-text-red-500(
+    icon="times"
+    @click="onClose"
+  )
+</template>
+
+<script>
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faTimes);
+
+export default {
+  components: {
+    "c-font-awesome-icon": FontAwesomeIcon
+  },
+  props: {
+    item: Object,
+    isSelected: Boolean
+  },
+  methods: {
+    onSelect() {
+      this.$emit("selected", this.item);
+    },
+    onClose() {
+      this.$emit("close", this.item);
+    }
+  }
+};
+</script>
