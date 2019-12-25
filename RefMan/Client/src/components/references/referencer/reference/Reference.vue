1ccf814d069ee22e1e48ea163292c90f137ff7f6
Aleksbgbg <kozichka01@abv.bg>
2019-12-25 12:17:28 +0000

    [Reference.vue] Add reference component, which shows the relevant citations, with associated action buttons.

---
+++ RefMan/Client/src/components/references/referencer/reference/Reference.vue
@@ -0,0 +1,87 @@
+<template lang="pug">
+.flex
+  .flex.flex-col.justify-end.mr-3
+    c-font-awesome-button.my-1(icon="edit" @click="toggleEdit")
+    c-font-awesome-button.my-1(icon="times" @click="deleteReference")
+    a(:href="reference.url" target="_blank")
+      c-font-awesome-button.my-1(icon="external-link-alt")
+  .flex.flex-col.items-center.text-sm
+    figure
+      img(
+        :src="reference.icon"
+        alt=""
+        height="25"
+        width="25"
+      )
+    h2 {{ reference.pageTitle }}
+    div
+      c-citation(title="In-text:")
+        | ({{ reference.pageTitle }}, {{ publishYear }})
+      c-citation(title="Bibliography:")
+        | {{ reference.websiteName }}.
+        | ({{ publishYear }}).
+        | {{ reference.pageTitle }}.
+        | [online]
+        | Available at: {{ reference.url }}
+        | [Accessed {{ accessDateReferenceFormat }}].
+</template>
+
+<script>
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEdit, faTimes, faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+import FontAwesomeButtonComponent from "@/components/shared/buttons/FontAwesomeButton";
+import CitationComponent from "@/components/references/referencer/reference/Citation";
+
+library.add(faEdit);
+library.add(faTimes);
+library.add(faExternalLinkAlt);
+
+export default {
+  components: {
+    "c-font-awesome-button": FontAwesomeButtonComponent,
+    "c-citation": CitationComponent
+  },
+  props: {
+    reference: Object
+  },
+  computed: {
+    publishYear() {
+      const publishYear = this.reference.publishYear;
+      return publishYear === null ? "n.d." : publishYear;
+    },
+    accessDateReferenceFormat() {
+      const date = this.reference.accessDate;
+
+      const day = date.getDate();
+      const monthIndex = date.getMonth();
+      const year = date.getFullYear();
+
+      const monthString = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ][monthIndex];
+      const monthThreeLetters = monthString.substring(0, 3);
+
+      const yearLastTwoDigits = year % 100;
+
+      return `${day} ${monthThreeLetters}. ${yearLastTwoDigits}`;
+    }
+  },
+  methods: {
+    toggleEdit() {
+    },
+    deleteReference() {
+    }
+  }
+};
+</script>
