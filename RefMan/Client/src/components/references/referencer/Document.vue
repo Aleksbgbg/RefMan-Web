e5cbf17329a9c58c3c421225c26d3e5af223f77f
Aleksbgbg <kozichka01@abv.bg>
2019-12-25 12:21:32 +0000

    [Document.vue] Add document component with a search bar and a list of references.

---
+++ RefMan/Client/src/components/references/referencer/Document.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+.p-2
+  c-search(@search="search")
+  .my-5
+    template(v-for="reference of references")
+      c-reference(:reference="reference")
+</template>
+
+<script>
+import SearchComponent from "@/components/references/referencer/Search";
+import ReferenceComponent from "@/components/references/referencer/reference/Reference";
+
+export default {
+  components: {
+    "c-search": SearchComponent,
+    "c-reference": ReferenceComponent
+  },
+  props: {
+    file: Object
+  },
+  data() {
+    return {
+      references: []
+    };
+  },
+  methods: {
+    search(query) {
+    }
+  }
+};
+</script>
