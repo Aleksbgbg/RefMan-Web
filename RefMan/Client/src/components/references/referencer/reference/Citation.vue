082e1a436607a5d4918c151215e689682a1fda4f
Aleksbgbg <kozichka01@abv.bg>
2019-12-25 12:13:07 +0000

    [Citation.vue] Add citation component, for in-text/bibliography citations.

---
+++ RefMan/Client/src/components/references/referencer/reference/Citation.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+div
+  h3.font-semibold {{ title }}
+  cite.inline-block.select-all.not-italic.ml-2
+    slot
+</template>
+
+<script>
+export default {
+  props: {
+    title: String
+  }
+};
+</script>
