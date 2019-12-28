f7c13c84b6242cd4669f08f0329e8fda89c96004
Aleksbgbg <kozichka01@abv.bg>
2019-12-28 15:30:37 +0000

    [PreventEntryFactory] Import state to prevent having to wait until the component is created to check for login access.

--- RefMan/Client/src/mixins/access-control/PreventEntryFactory.js
+++ RefMan/Client/src/mixins/access-control/PreventEntryFactory.js
@@ -1,18 +1,15 @@
-import { mapState } from "vuex";
+import accountModule from "@/store/account";
+
+const state = accountModule.state;
 
 export function createMixin(allowedToAccess, computeRedirect) {
   return {
-    computed: mapState({
-      isLoggedIn: (state) => state.account.isLoggedIn
-    }),
     beforeRouteEnter(to, from, next) {
-      next((instance) => {
-        if (allowedToAccess(instance.isLoggedIn)) {
-          next();
-        } else {
-          next(computeRedirect(to, from, next));
-        }
-      });
+      if (allowedToAccess(state.isLoggedIn)) {
+        next();
+      } else {
+        next(computeRedirect(to, from, next));
+      }
     }
   };
 }
