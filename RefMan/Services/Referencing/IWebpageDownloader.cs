99488f55014f3267223dbb46e2659eee4967e1e8
Aleksbgbg <kozichka01@abv.bg>
2020-01-13 08:16:57 +0000

    [WebpageDownloader] Add service to download a HTML page from a URL and convert this to IWebpage.

---
+++ RefMan/Services/Referencing/IWebpageDownloader.cs
@@ -0,0 +1,11 @@
+﻿namespace RefMan.Services.Referencing
+{
+    using System.Threading.Tasks;
+
+    using RefMan.Services.Referencing.PageSearching;
+
+    public interface IWebpageDownloader
+    {
+        Task<IWebpage> DownloadWebpageFromUrl(string url);
+    }
+}
---
+++ RefMan/Services/Referencing/WebpageDownloader.cs
@@ -0,0 +1,19 @@
+﻿namespace RefMan.Services.Referencing
+{
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    using RefMan.Services.Referencing.PageSearching;
+
+    public class WebpageDownloader : IWebpageDownloader
+    {
+        public async Task<IWebpage> DownloadWebpageFromUrl(string url)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string webpageHtml = await httpClient.GetStringAsync(url);
+                return Webpage.FromHtml(webpageHtml);
+            }
+        }
+    }
+}
